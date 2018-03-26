4873c53aaf0e92f9d2cd98e495cdbac7b6dcdf12
Yang Zhongdong <yangzd1996@outlook.com>
2018-03-26 21:36:21 +0800

    Update/Add: route pages and add font link to each page

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,8 @@ window.onload = function () {
         if (ev.target !== main) return;
         window.requestAnimationFrame(() => {
             if (main.scrollTop <= 200) {
-                header_div_logo.style.left = (200 - main.scrollTop) / 200 * (document.body.clientWidth / 2 - 0.5 * header_div_logo.clientWidth) + 'px';
+                let value = (200 - main.scrollTop) / 200 * (document.body.clientWidth / 2 - 0.5 * header_div_logo.clientWidth);
+                header_div_logo.style.left = (value >= 16 ? value : 16) + 'px';
             } else if (main.scrollTop > 200) {
                 header_div_logo.style.left = 16 + 'px';
             } else {
@@ -35,14 +36,14 @@ window.onload = function () {
             <span><a href="../index.html">首页</a></span>
             <span><a href="./docs.html">文档</a></span>
         </div>`;
-        div.innerHTML = `<hr><a href='https://github.com/DevChache/serverhub-mvc/'>访问 GitHub 源码仓库</a>`;
+        div.innerHTML = `<hr><a href='https://github.com/ServerHubOrg/serverhub-mvc/'>访问 GitHub 源码仓库</a>`;
     }
     else {
         nav.innerHTML = `<div class="nav">
             <span><a href="../index.html">Home</a></span>
             <span><a href="./docs.html">Docs</a></span>
         </div>`;
-        div.innerHTML = `<hr><a href='https://github.com/DevChache/serverhub-mvc/'>View on GitHub</a>`
+        div.innerHTML = `<hr><a href='https://github.com/ServerHubOrg/serverhub-mvc/'>View on GitHub</a>`
     }
     if (document.querySelector('#nonav') === null)
         content.insertBefore(nav, content.firstChild);
